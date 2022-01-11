c94db3eb70602687f0523555cb007bc1b26a52ac
Theemiss <midinfotn401@gmail.com>
2022-01-11 20:16:06 +0100

    fix

--- 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -6,10 +6,13 @@ public class WinTrigger : MonoBehaviour
     public Transform Player;
     private Timer time;
     private Text timeText;
+    public Canvas Winncanvas;
+    public Text WinText;
 
     private void Start()
     {
         time = Player.GetComponent<Timer>();
+        Winncanvas.enabled = false;
         timeText = time.TimerText;
     }
     private void OnTriggerEnter(Collider win)
@@ -17,6 +20,8 @@ public class WinTrigger : MonoBehaviour
         if (win.tag == "Player")
         {
             fuckingwin();
+            WinText.text = timeText.text;
+            Winncanvas.enabled = true;
         }
     }
     public void fuckingwin()
@@ -24,6 +29,10 @@ public class WinTrigger : MonoBehaviour
         timeText.fontSize = 80;
         timeText.color = Color.green;
         time.timerchecker(false);
+     
+        
+        
+
     }
 
 }
